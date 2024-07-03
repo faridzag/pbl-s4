d38286595ad82a983dc5816bc165f55fce23f529
faridzag <faridzarhamgustin@gmail.com>
2024-07-03 12:25:59 +0700

    add link card in dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,38 @@
     @endif
 
     <div class="row">
-        <h2 class="h4 mb-4 text-gray-600">{{ __('Halo, ') }}{{ $user->fullname }}</h1>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold .text-primary-emphasis text-uppercase mb-1">{{ __('Halo, ') }}{{ $user->fullname }}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">-</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-smile fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+            <a href="{{ route('landing') }}">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jelajahi Job Fair</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">-</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-search fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</a>
+        </div>
     </div>
 @endsection
