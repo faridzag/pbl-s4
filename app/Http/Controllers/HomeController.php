6282c4fe8b2310fb36e3cf34ceb2973cd8460bc3
faridzag <faridzarhamgustin@gmail.com>
2024-10-04 05:50:07 +0700

    fix: displayed vacancy and application count

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,22 +31,28 @@ class HomeController extends Controller
             ];
 
             $events = Event::withCount(['companies', 'jobVacancies', 'jobApplications'])
-                ->with(['jobApplications' => function ($query) {
-                    $query->select('event_id', 'status')
-                        ->selectRaw('COUNT(*) as total')
-                        ->groupBy('event_id', 'status');
-                }])
+                ->withCount([
+                    'jobApplications as pending_count' => function ($query) {
+                        $query->where('status', 'pending');
+                    },
+                    'jobApplications as reject_count' => function ($query) {
+                        $query->where('status', 'reject');
+                    },
+                    'jobApplications as accept_count' => function ($query) {
+                        $query->where('status', 'accept');
+                    }
+                ])
                 ->get()
                 ->map(function ($event) {
                     $applicationStatus = $event->jobApplications->pluck('total', 'status')->toArray();
                     return [
                         'name' => $event->name,
                         'companies_count' => $event->companies_count,
-                        'jobs_count' => $event->jobVacancies_count,
-                        'applications_count' => $event->jobApplications_count,
-                        'pending_count' => $applicationStatus['pending'] ?? 0,
-                        'reject_count' => $applicationStatus['reject'] ?? 0,
-                        'accept_count' => $applicationStatus['accept'] ?? 0,
+                        'jobs_count' => $event->job_vacancies_count,
+                        'applications_count' => $event->job_applications_count,
+                        'pending_count' => $event->pending_count ?? 0,
+                        'reject_count' => $event->reject_count ?? 0,
+                        'accept_count' => $event->accept_count ?? 0,
                         'event_type' => $event->event_type,
                         'status' => $event->status,
                         'start_date' => $event->start_date,
