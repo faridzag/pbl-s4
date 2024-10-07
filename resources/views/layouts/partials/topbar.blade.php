8f9f19df714c018cf99930f48e24fe4968187baa
faridzag <faridzarhamgustin@gmail.com>
2024-10-07 15:35:52 +0700

    fix topbar avatar

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -141,7 +141,11 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->username }}</span>
-                <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->username[0] }}"></figure>
+                    @if (isset(Auth::user()->avatar))
+                        <img src="{{ asset(str_replace('public/', '', 'storage/' . Auth::user()->avatar)) }}" alt="{{ Auth::user()->name }}"width="40" height="40" class="rounded-circle shadow-4-strong">
+                    @else
+                        <figure class="img-profile rounded-circle avatar font-weight-bold" data-initial="{{ Auth::user()->username[0] }}"></figure>
+                    @endif
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
