4129db9e6c0edc285dc51e2bdd7200e78f141116
faridzag <faridzarhamgustin@gmail.com>
2024-10-14 10:43:44 +0700

    fix event name search in company JobApplication

--- app/Http/Controllers/COMPANY/JobApplicationController.php
+++ app/Http/Controllers/COMPANY/JobApplicationController.php
@@ -13,22 +13,24 @@ class JobApplicationController extends Controller
         $companyId = auth()->user()->company->id;
         $applications = Application::where('company_id', $companyId)
             ->with('applicant.user', 'vacancy.event');
-        
-            
+
+
         if ($request->has('search') && $request->search !== '') {
             $applications = $applications->whereHas('applicant.user', function($query) use ($request) {
                 $query->where('name', 'LIKE', '%' . $request->search . '%');
             })->orWhereHas('vacancy', function($query) use ($request) {
                 $query->where('position', 'LIKE', '%' . $request->search . '%');
+            })->orWhereHas('event', function ($query) use ($request) {
+                $query->where('name', 'LIKE', '%' . $request->search . '%');
             });
         }
-        
+
         $applications = $applications->paginate(10);
         return view('pages.job-application.list', compact('applications'));
     }
 
     public function update(Request $request, $id)
-    {    
+    {
         $application = Application::findOrFail($id);
         $application->update(['status' => $request->status]);
         return redirect()->back()->with('success', 'Lamaran berhasil diupdate!');
