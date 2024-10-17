b70b2e897945498725a6dbba4fe93909d732508d
faridzag <faridzarhamgustin@gmail.com>
2024-10-18 06:38:05 +0700

    add mockup test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    public function test_login_validation()
+    {
+        // Test empty fields
+        $response = $this->post('/login', []);
+        $response->assertSessionHasErrors(['login', 'password']);
+
+        // Test with only email
+        $response = $this->post('/login', ['login' => 'test@gmail.com']);
+        $response->assertSessionHasErrors(['password']);
+    }
+
+    public function test_login_type_branches()
+    {
+        // Test email branch
+        $response = $this->post('/login', [
+            'login' => 'jpcuser@mail.me',
+            'password' => '123456'
+        ]);
+        $response->assertSessionHasErrors();
+        //$response->assertRedirect('home');
+
+        // Test username branch
+        $response = $this->post('/login', [
+            'login' => 'jpcuser',
+            'password' => '12345678'
+        ]);
+        $response->assertSessionHasErrors();
+    }
+}
